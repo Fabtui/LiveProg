7134ee0f7f81e333846d68f7a45460c0ac2d4fda
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-25 17:24:04 +0200

    event sorted

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
   belongs_to :bar
   has_many :participations
   validates :start_date, presence: true
+
+  scope :future, -> { where(start_date: DateTime.now..2.years.from_now) }
+  scope :sorted_by_date, -> { order(start_date: :asc) }
 end
