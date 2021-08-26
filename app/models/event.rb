c238151c0f088abfd6a16a8581f0fc91892a03b8
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-26 15:29:42 +0200

    delete add to participation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   belongs_to :bar
   has_many :participations
   validates :start_date, presence: true
+  validates :bar_id, presence: true, uniqueness: { scope: :start_date }
 
   scope :future, -> { where(start_date: DateTime.now..2.years.from_now) }
   scope :sorted_by_date, -> { order(start_date: :asc) }
