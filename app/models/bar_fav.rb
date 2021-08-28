c49ebf8328fb5958f33cba8acaafbf1a2918ec8f
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-28 13:17:54 +0200

    display farovite bar on home page

--- app/models/bar_fav.rb
+++ app/models/bar_fav.rb
@@ -1,4 +1,6 @@
 class BarFav < ApplicationRecord
   belongs_to :user
   belongs_to :bar
+
+  validates :bar_id, presence: true, uniqueness: { scope: :user_id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
     resources :events, only: [:new, :create]
   end
 
-  resources :bars, only: [:index, :show]
+  resources :bars, only: [:index, :show] do
+    resources :bar_favs, only: [:create]
+  end
+
   resources :events, except: [:new, :create] do
     resources :participations, only: [ :create ]
   end
 
+  resources :bar_favs, only: :destroy
   resources :participations, only: :destroy
   devise_for :users
   root to: 'pages#home'
