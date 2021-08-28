287a5491ef90db824e27da841ff4b989360a58f3
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-28 09:55:51 +0200

    fixe popup

--- app/javascript/components/participationpopup.js
+++ app/javascript/components/participationpopup.js
@@ -1,19 +1,11 @@
 import { preventInsignificantClick } from "@rails/ujs"
 
-const participationPopUp = () => {
+const participationPopUpClose = () => {
   const container = document.querySelector('#infos-container')
   const close = document.getElementById('participation-popup-close')
   const addParticipation = document.getElementById('add-participation')
   const popUp = document.getElementById('participation-popup')
-  // addParticipation.addEventListener('click', (e) => {
-  //   popUp.classList.remove('hidden')
-  //   container.style.opacity = "0.5"
-  //   // container.style.filter = "blur(3px)";
-  // }
-  // )
-
   close.addEventListener('click', (e) => {
-    // event.preventDefault();
     popUp.classList.add('hidden')
     popUp.style.transition = "0.5s";
     container.style.opacity = ""
@@ -22,4 +14,4 @@ const participationPopUp = () => {
   )
 }
 
-export { participationPopUp }
+export { participationPopUpClose }
