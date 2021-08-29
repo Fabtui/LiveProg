5c65955e6ff4eb2f23d4795510ebc8531fc78ad0
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-29 18:04:51 +0200

    add comment to band

---
+++ app/controllers/band_reviews_controller.rb
@@ -0,0 +1,33 @@
+class BandReviewsController < ApplicationController
+
+  def new
+    @band = Band.find(params[:band_id])
+    @band_review = BandReview.new
+  end
+
+  def create
+    @band_review = BandReview.new(band_review_params)
+    @band = Band.find(params[:band_id])
+    @band_review.band_id = @band.id
+    @band_review.user_id = current_user.id
+    if @band_review.save
+      redirect_to band_path(@band)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @band_review = BandReview.find(params[:id])
+    @band = Band.find_by_id(@band_review.band_id)
+    @band_review.destroy
+
+    redirect_to band_path(@band)
+  end
+
+  private
+
+  def band_review_params
+    params.require(:band_review).permit(:comment, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :events, only: [:new, :create]
   end
 
+  resources :bands do
+    resources :band_reviews, only: [:new, :create]
+  end
+
   resources :bars, only: [:index, :show] do
     resources :bar_favs, only: [:create]
   end
@@ -11,6 +15,7 @@ Rails.application.routes.draw do
     resources :participations, only: [ :create ]
   end
 
+  resources :band_reviews, only: :destroy
   resources :bar_favs, only: :destroy
   resources :participations, only: :destroy
   devise_for :users
