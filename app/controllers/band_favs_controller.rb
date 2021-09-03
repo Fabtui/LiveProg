ce83a6bf45dda964f24c55235b4d43a30928395b
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-09-03 12:07:58 +0200

    favband add

--- app/controllers/band_favs_controller.rb
+++ app/controllers/band_favs_controller.rb
@@ -1,9 +1,12 @@
 class BandFavsController < ApplicationController
   def create
+    @band = Band.find(params[:band_id])
     @band_fav = BandFav.new
     @band_fav.user_id = current_user.id
     @band_fav.band_id = params[:band_id]
     @band_fav.save
+
+    redirect_to band_path(@band)
   end
 
   def destroy
