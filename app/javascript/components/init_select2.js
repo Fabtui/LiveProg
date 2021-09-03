07c80c69f88bce48602e3a1be10d12f7fca601d7
Sebastien Rousseau <sebounoob@gmail.com>
2021-09-03 11:09:31 +0200

    tag persiste et placeholder ok

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,6 +2,13 @@ import $ from 'jquery'
 import 'select2'
 
 const initSelect2 = () => {
-    $('#search_band_style').select2()
+  $('#search_band_style').select2()
+
+  $('.select2-selection')
+    .prepend("Par style")
+    .addClass("form-control")
+    .css("font-family", "Nunito")
+    .css("color", "#6d757d")
+
 }
 export { initSelect2 }
